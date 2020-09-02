d35e655bcf259d2032e76b6953069ccde7c37eb0
rafael_lucio <rafaellucio.developer@gmail.com>
2020-09-02 18:59:53 -0300

    projeto todo app, curso js basico rockeseat

---
+++ script.js
@@ -0,0 +1,46 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos() {
+  listElement.innerHTML = '';
+  for (todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+    var pos = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
+    todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+    listElement.appendChild(todoElement);
+  }
+}
+
+function addTodo() {
+  var textTodo = inputElement.value + ' - ';
+  todos.push(textTodo);
+  inputElement.value = '';
+  renderTodos();
+  saveStorage();
+}
+
+function deleteTodo(pos) {
+  todos.splice(pos, 1);
+  renderTodos();
+  saveStorage();
+}
+
+function saveStorage() {
+  localStorage.setItem('list_todos', JSON.stringify(todos));
+}
+
+buttonElement.onclick = addTodo;
+
+renderTodos();
